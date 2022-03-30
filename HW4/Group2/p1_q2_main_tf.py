82b3e4c55956e0ef9eb09cd75413bf9f282a8be9
Siyeol Choi <siyeol13@naver.com>
2022-03-29 20:06:16 -0500

    Submission Files

---
+++ HW4/Group2/p1_q2_main_tf.py
@@ -0,0 +1,125 @@
+import tensorflow as tf
+import argparse
+import matplotlib.pyplot as plt
+from torchsummary import summary
+import numpy as np
+import time
+
+
+
+# Argument parser
+parser = argparse.ArgumentParser(description='EE379K HW4 - Starter TensorFlow code')
+# Define the mini-batch size, here the size is 128 images per batch
+parser.add_argument('--batch_size', type=int, default=128, help='Number of samples per mini-batch')
+# Define the number of epochs for training
+parser.add_argument('--epochs', type=int, default=100, help='Number of epoch to train')
+# Define the model class
+parser.add_argument('--model_class', type=str, default="VGG11", help='Model to train')
+args = parser.parse_args()
+
+epochs = args.epochs
+batch_size = args.batch_size
+model_to_train = args.model_class
+
+random_seed = 1
+tf.random.set_seed(random_seed)
+
+# TODO: Insert your model here
+if (model_to_train == "VGG11"):
+    from models.vgg11_tf import VGG
+elif (model_to_train == "VGG16"):
+    from models.vgg16_tf import VGG
+
+
+# used to limit the memory growth
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
+
+# use GPU
+with tf.device("/device:GPU:0"):
+
+
+    model = VGG()
+    print(model.summary())
+
+    # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # use cuda if available
+    # model.to(device)        # put model to cuda
+    # summary(model, (3, 32, 32))
+
+
+    # TODO: Load the training and testing datasets
+
+    (train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+    # class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+    # Prepare the training dataset.
+    # train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+    # train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+
+    # # Prepare the testing dataset.
+    # test_dataset = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+    # test_dataset = test_dataset.batch(batch_size)
+
+
+
+    # TODO: Convert the datasets to contain only float values
+    train_images_norm = train_images.astype('float32')
+    test_images_norm = test_images.astype('float32')
+
+    # TODO: Normalize the datasets
+    train_images_norm = train_images_norm/255.0
+    test_images_norm = test_images_norm/255.0
+
+    # TODO: Encode the labels into one-hot format
+    train_labels = tf.keras.utils.to_categorical(train_labels)
+    test_labels = tf.keras.utils.to_categorical(test_labels)
+
+    # train_labels_onehot = tf.one_hot(train_labels, depth=len(np.unique(y_train)))
+    # test_labels_onehot = tf.one_hot(test_labels, depth=len(np.unique(y_train)))
+
+    # TODO: Configures the model for training using compile method
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), loss = 'categorical_crossentropy', metrics=['accuracy'])
+
+    # TODO: Train the model using fit method
+    start = time.time()
+    history = model.fit(train_images_norm, train_labels, epochs=epochs, batch_size=batch_size, validation_data=(test_images_norm, test_labels), verbose=1)
+    end = time.time()
+
+    total_training_time = end - start
+
+    print("Total time: ", total_training_time, "seconds")
+
+
+    # plt.plot(history.history['accuracy'], label='accuracy')
+    # plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    # plt.xlabel('Epoch')
+    # plt.ylabel('Accuracy')
+    # plt.grid()
+    # plt.legend(loc='lower right')
+
+    test_loss, test_acc = model.evaluate(test_images_norm,  test_labels, verbose=2)
+    print("test_acc :", test_acc)
+    print(history.history['val_accuracy'])
+
+    # TODO: Save the weights of the model in .ckpt format
+    if (model_to_train == "VGG11"):
+        # plt.title("VGG11")
+        # plt.savefig("vgg11_accplot.png")
+        #model.save("./models/vgg11/vgg11.h5")
+        model.save_weights("./models/vgg11/vgg11_tf.ckpt")
+
+    elif (model_to_train == "VGG16"):
+        # plt.title("VGG16")
+        # plt.savefig("vgg16_accplot.png")
+        #model.save("./models/vgg16/vgg16.h5")
+        model.save_weights("./models/vgg16/vgg16_tf.ckpt")
---
+++ HW4/Group2/p2_q3_plot_vgg11.py
@@ -0,0 +1,78 @@
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+times_rsp=[]
+powers_rsp=[]
+temps_rsp=[]
+
+times_mc1=[]
+powers_mc1=[]
+temps_mc1=[]
+
+runpow_rsp=[]
+runpow_mc1=[]
+
+df_rsp = open("./rsp/log_raspberry_vgg11.csv")
+df_mc1 = open("./mc1/log_mc1_vgg11.csv")
+
+df_rsp.readline()
+df_mc1.readline()
+
+for line in df_rsp:
+    (time_stamp, total_power, temps_avg, temp) = line.split(',')
+    xtime = float(time_stamp) - 1648517339.3162882
+    times_rsp.append(float(xtime))
+    powers_rsp.append(float(total_power))
+    temps_rsp.append(float(temps_avg))
+    if float(time_stamp) >= 1648517339.9171257 and float(time_stamp) <= 1648519506.9401393:
+        runpow_rsp.append(float(total_power))
+
+for line in df_mc1:
+    (time_stamp, total_power, temps_avg, temp) = line.split(',')
+    xtime = float(time_stamp) - 1648582501.050831
+    times_mc1.append(float(xtime))
+    powers_mc1.append(float(total_power))
+    temps_mc1.append(float(temps_avg))
+    if float(time_stamp) >= 1648582501.8518972 and float(time_stamp) <= 1648583406.3017142:
+        runpow_mc1.append(float(total_power))
+
+sum_runpow_rsp = sum(runpow_rsp)
+sum_runpow_mc1 = sum(runpow_mc1)
+
+print(len(runpow_rsp), len(runpow_mc1))
+
+print("RSP energy (J) :", sum_runpow_rsp * (0.2))
+print("MC1 energy (J) :", sum_runpow_mc1 * (0.2))
+
+power_plot = plt.figure(1, figsize=(20,5))
+plt.plot(times_mc1, powers_mc1, label='MC1 [W]')
+plt.plot(times_rsp, powers_rsp, label='RSP [W]')
+plt.title("System Power Consumption")
+plt.xlabel("time[s]")
+plt.ylabel("Power[W]")
+plt.legend()
+plt.grid()
+plt.savefig("power_plot_vgg11.png")
+
+
+power_plot = plt.figure(2, figsize=(20,5))
+plt.plot(times_mc1, temps_mc1, label='MC1 [°C]')
+plt.plot(times_rsp, temps_rsp, label='RSP [°C]')
+plt.title("Temperature")
+plt.xlabel("time[s]")
+plt.ylabel("Temperature[°C]")
+plt.legend()
+plt.grid()
+plt.savefig("temp_plot_vgg11.png")
+
+
+# rsp vgg 11
+# start:  1646070329.88807
+# end:  1646071278.378308
+# Test Accuracy :  74.48
+# Total time for inference : 915.9830 seconds
+# Sec/Acc :  12.298374884750865
+
+# mc1 vgg 11
+# start: 
